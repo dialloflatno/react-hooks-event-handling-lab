64b252f7fc0611b003c3b7c34e5f7f54306cef5a
dialloflatno <brahimdiallo23@gmail.com>
2021-11-06 22:42:01 -0400

    Completed

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,21 @@
-// Code EyesOnMe Component Here
+
+function EyeOnMe() {
+
+    function handleFocus() { return console.log('Good!'); }
+    function handleBlur() { return console.log('Hey! Eyes on me!'); }
+
+
+
+    return (
+        <div>
+
+            <button onFocus={handleFocus}
+                onBlur={handleBlur}>EyeOnMe</button>
+
+        </div>
+    )
+
+}
+
+
+export default EyeOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,17 @@
-// Code Keypad Component Here
+
+function Keypad() {
+ function handleChange(){
+     return console.log('Entering password...');
+ }
+
+    return (
+        <div>
+
+<input type="text" onChange = {handleChange}></input>
+        </div>
+    )
+
+}
+
+
+export default Keypad;
